da6f2f410338fcc25464ee723c7d4e22997b670b
Nilesh <nileshp@intellihinc.com>
2020-04-11 11:30:02 +0530

    Create FragmentThree.java

---
+++ java/com/endo/intellih/activities/FragmentThree.java
@@ -0,0 +1,26 @@
+package com.endo.intellih.activities;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.endo.intellih.R;
+
+public class FragmentThree extends Fragment {
+ 
+    public static Fragment newInstance(Context context) {
+    	FragmentThree f = new FragmentThree();
+ 
+        return f;
+    }
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_three, null);
+        return root;
+    }
+ 
+}
